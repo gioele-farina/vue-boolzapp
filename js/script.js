627e8cdfed8c0ea25cfc5dd49ecdc7f014f27f06
Gioele Farina <jchilosa@gmail.com>
2020-11-22 10:54:45 +0100

    collego il layout a vue parte1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+var app =new Vue({
+  el:'#app',
+  data: {
+    // GRAFICA APP (layout-desktop layout-contatti-mobile layout-chat-mobile)
+    layoutApp: "layout-desktop",
+    // MACROSEZIONE MAIN (start chat)
+    schermataMaim: "chat",
+    // SEZIONI (booleane)
+    nav: true,
+    contactsAlert: true,
+    userInfo: true,
+    contacts: true,
+    chatInfo: true
+  },
 
+  mounted: function () {
+    this.$nextTick(function () {
+      this.nav = false;
+      // this.schermataMaim = "start";
+    })
+  },
+
+  methods: {
+
+  }
+});
